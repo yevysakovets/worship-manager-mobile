de6413e68a82eef84de7aab880cc7d9afe96d573
yevysakovets <zhenyasakovets@gmail.com>
2022-07-19 11:21:55 -0500

    initial commit

---
+++ ios/Pods/Target Support Files/Pods-worshipManager-worshipManagerTests/Pods-worshipManager-worshipManagerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_worshipManager_worshipManagerTests : NSObject
+@end
+@implementation PodsDummy_Pods_worshipManager_worshipManagerTests
+@end
